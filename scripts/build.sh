e89050b92d2ba05c3835524f929363d44be0c06d
edneifneto <edneifneto@gmail.com>
2021-06-07 09:29:18 -0300

    Add git command

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,25 +11,37 @@ cr=$(echo $'\n> ')
 cr=${cr%.}
 
 # shellcheck disable=SC2162
-read -p "\nDeseja criar Aplicativo[S/N]?: $cr" CONTINUE
+read -p "Deseja criar Aplicativo[S/N]: $cr" CONTINUE
 
 # shellcheck disable=SC1009
 if [ "$CONTINUE" = "s" ]
 then
     cd ..
 
-    printf "\n\nCleaning ...\n"
-    ./gradlew clean
+    # printf "\n\nCleaning ...\n"
+    # ./gradlew clean
 
-    printf "\n\nCleaning build cache ...\n"
-    ./gradlew cleanBuildCache
+    # printf "\n\nCleaning build cache ...\n"
+    # ./gradlew cleanBuildCache
 
-    printf "\n\nCreate APK ...\n"
-    ./gradlew assembleDebug
+    # printf "\n\nCreate APK ...\n"
+    # ./gradlew assembleDebug
+
 
     printf "\n\nCopping...\n"
-    cp -Rf "app/build/outputs/apk/debug" "/c/Users/${USER}/Desktop/Dev/"
+    mkdir -p "app/src/release" && cp -Rf "app/build/outputs/apk/debug" "app/src/release"
     printf "\n\nSUCCESS\n"
+
+    git add app/src/release
+
+    git status
+
+    git commit -m "Add new release"
+
+    git push -u origin main
+
+    printf "\n\nSUCCESS\n"
+
 else
   printf "\n\nOPERCAO CANCELADA"
 fi
